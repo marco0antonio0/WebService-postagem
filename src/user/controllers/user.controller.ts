51bbc8458146f05cc66c48cf321167d45875a1f9
marco0antonio0 <marcomesquitajr@hotmail.com>
2024-08-06 14:53:14 -0300

    protect routers

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -2,11 +2,12 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UnauthorizedException } from '@nestjs/common';
 import { user_loginDTO } from '../models/user-models.dto';
 import { UserService } from '../services/user.service';
 import { userDTO } from '../models/user.dto';
 import { json } from 'sequelize';
+require('dotenv').config()
 
 @Controller("user")
 export class UserController {
@@ -21,8 +22,13 @@ export class UserController {
     @Post("register")
     @HttpCode(201)
     async register(@Body() user: userDTO) {
-        const data = await this.userService.regiser(user)
-        return { token: data }
+        if (process.env.PROD) {
+            throw new UnauthorizedException("Disable router")
+        } else {
+            const data = await this.userService.regiser(user)
+            return { token: data }
+        }
+
     }
 
 }
