a8bbc273afa201bd1519cbd9846ef2e14eaeb9ac
marco0antonio0 <marcomesquitajr@hotmail.com>
2024-08-07 12:57:47 -0300

    up

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: '*', // Permitir solicitações de qualquer origem
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
+    credentials: true, // Permitir credenciais (como cookies, tokens de autenticação)
+    // Você pode adicionar outras opções de configuração aqui, se necessário
+  };
+
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Limite de requisições excedido. Tente novamente mais tarde.',
+  });
+  app.use(limiter);
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
